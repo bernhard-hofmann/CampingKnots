f378e25000a7963d530d67dfa9548f672c9fb31d
Bernhard Hofmann <bhofmann@hotmail.co.uk>
2011-09-30 10:04:16 +0100

    Initial commit with WIP and initial (figure eight) knot images.

---
+++ assets/js/app.js
@@ -0,0 +1,48 @@
+function bizt() {
+	if (navigator && navigator.notification && navigator.notification.vibrate) {
+		navigator.notification.vibrate(30);
+	}
+}
+function onMenuKeyDown() {
+	$.mobile.changePage( "menu", { transition: "none"} );
+}
+function onSearchKeyDown() {
+	$.mobile.changePage( "info", { transition: "none"} );
+}
+
+function initApp() {
+	var activity = 'click';
+	if ('ontouchstart' in document.documentElement) {
+		activity = 'touchstart';
+	};
+
+	if (typeof(device) !== "undefined") {
+		$('#phoneGapVersion').html('version '+ device.phonegap);
+		var element = document.getElementById('deviceProperties');
+		if (element) {
+			element.innerHTML =
+				'Device: '     + device.name     + '<br />' + 
+				'Running: ' + device.platform +' v'+ device.version + '<br />' + 
+				'UUID: '     + device.uuid     + '<br />'
+				;
+		}
+	}
+
+	document.addEventListener("menubutton", onMenuKeyDown, false);
+	document.addEventListener("searchbutton", onSearchKeyDown, false);
+}
+/*
+document.addEventListener("backbutton", onBackKeyDown, false);
+document.addEventListener("menubutton", onMenuKeyDown, false);
+document.addEventListener("pause", onPause, false);
+document.addEventListener("resume", onResume, false);
+function onBackKeyDown() {
+	alert('Back button not supported.');
+}
+function onPause() {
+	// Handle the pause event
+}
+function onResume() {
+	alert('Welcome back! :D');
+}
+*/
